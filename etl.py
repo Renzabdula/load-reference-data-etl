2d08f8e8a13391dfe19f1f2942b040c75a5dd916
Renz <renzabdula1995@gmail.com>
2024-04-26 11:00:08 +0800

    added function to remove the loaded file to sql

--- etl.py
+++ etl.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import os
+from os import remove
+from pathlib import Path
 
 
-def extract():
+def file_to_delete(file_source: str | Path):
+    remove(file_source)
+
+def extract(): 
     
     dir = r"C:\Users\Lyka Cedo\Desktop\Power BI\introduction-to-power-bi\Datasets\Contoso DW"
     try:
@@ -17,12 +22,12 @@ def extract():
                 if os.path.isfile(f):
                     df = pd.read_csv(f)
                     # call load function
-                    load(df, file_wo_ext)
+                    load(df, file_wo_ext, f)
 
     except Exception as e:
         print("Data error extraction of :", str(e))
 
-def load(df, tbl_name):
+def load(df, tbl_name, file_source):
     try:
         rows_imported = 0
         engine = create_engine("postgresql://etl:demopass@localhost:5432/db_demo")
@@ -31,6 +36,10 @@ def load(df, tbl_name):
         df.to_sql(f"stg_{tbl_name}", engine, if_exists='replace', index=False)
         rows_imported += len(df)
         print(f"data imported successfully")
+
+        file_to_delete(file_source)
+        print(f'{file_source} deleted')
+        
     
     except Exception as e:
         print("Data load error: ", str(e))
