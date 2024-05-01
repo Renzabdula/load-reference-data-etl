69b82978ad6e3d585974e67daba13b091b56f110
Renz <renzabdula1995@gmail.com>
2024-05-01 09:32:05 +0800

    added try and except block

--- etl_sql2pathfile.py
+++ etl_sql2pathfile.py
@@ -20,29 +20,41 @@ pw = os.environ['pwd']
 dir = r'C:\Users\Lyka Cedo\Desktop\Power BI'
 
 def extract():
-    connection_string = f"Driver={driver_name};Server={server_name};Database={db};Trusted_Connection=yes;uid={uid};pwd={pw}"
-    connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
-    engine = create_engine(connection_url, module=pyodbc)
-    print("Connected to database successfully")
+    
+    try:
+        connection_string = f"Driver={driver_name};Server={server_name};Database={db};Trusted_Connection=yes;uid={uid};pwd={pw}"
+        connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+        engine = create_engine(connection_url, module=pyodbc)
+        print("Connected to database successfully")
+    except Exception as e:
+        print("Connection error:", str(e))
 
-    src_query = """SELECT table_name FROM information_schema.tables"""
-    src_table = pd.read_sql_query(f'{src_query}', engine).to_dict()['table_name']
-    print(src_table)
+    try:
+        src_query = """SELECT table_name FROM information_schema.tables"""
+        src_table = pd.read_sql_query(f'{src_query}', engine).to_dict()['table_name']
+        print(src_table)
 
-    for table_name in src_table.values():
-        df = pd.read_sql_query(f'SELECT * FROM "{table_name}"',engine)
-        print(df.info())
-        print(df.shape)
-        load(df, table_name, dir)
-
+        for table_name in src_table.values():
+            df = pd.read_sql_query(f'SELECT * FROM "{table_name}"',engine)
+            print(df.info())
+            print(df.shape)
+            load(df, table_name, dir)
+    except Exception as e:
+        print("Data extract error:", str(e))
 
 def load(df, tbl, dir):
-    # create a file path
-    f = os.path.join(dir, tbl)
-    # load to CSV
-    df.to_csv(f'{f}.csv', index=False)
-    for file_name in os.listdir(dir):
-        print(file_name)
+    try:
+        # create a file path
+        f = os.path.join(dir, tbl)
+        # load to CSV
+        csv_file = f"{f}.csv"
+        df.to_csv(csv_file, index=False)
+        print("csv file successfully downloadd to:",f)
+    
+    except Exception as e:
+        print("Data load error:", str(e))
+
+        
         
 
 extract()
